58ca792617ba5d2af319847e576b72bd4f6ae814
Nate Berman <berman.nate@gmail.com>
2014-05-12 16:06:46 -0500

    configure for heroku

--- biorhythm/settings.py
+++ biorhythm/settings.py
@@ -81,3 +81,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
