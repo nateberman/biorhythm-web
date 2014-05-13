6d1d387c406a2a6306c573a701aa5a9437b00595
Nate Berman <berman.nate@gmail.com>
2014-05-13 13:54:35 -0500

    update decorator import

--- makewave/views.py
+++ makewave/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
 import json
 import math
 from datetime import date, datetime
