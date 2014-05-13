2595e8ab5749afccd5e77f7fad176425b219b695
Nate Berman <berman.nate@gmail.com>
2014-05-13 11:06:18 -0500

    add test suite. add view decorator require_get() to makewave

--- makewave/tests.py
+++ makewave/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.test import Client
 
 # Create your tests here.
+class MakewaveTestCase(TestCase):
+	def test_makewave(self):
+		"""GET a biorhythm wave"""
+		c = Client()
+		response = c.get('/makewave/', {'year':1982, 'month':03, 'day':10})
+		self.assertEqual(response.status_code, 200)
+
+	def test_makewave(self):
+		"""A POST to makewave should fail"""
+		c = Client()
+		response = c.post('/makewave/', {'year':1982, 'month':03, 'day':10})
+		self.assertEqual(response.status_code, 404)	
